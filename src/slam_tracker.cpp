962996683735a83eb318f636a52b0d78a2641d99
Mateo de Mayo <mateo.demayo@collabora.com>
2021-10-12 16:32:38 -0300

    Implement slam_tracker queues, initially working

--- src/slam_tracker.cpp
+++ src/slam_tracker.cpp
@@ -3,14 +3,14 @@
 #include <CLI/CLI.hpp>
 
 #include <stdio.h>
+#include <memory>
 #include <string>
 
-#include <basalt/vi_estimator/vio_estimator.h>
 #include <basalt/serialization/headers_serialization.h>
+#include <basalt/vi_estimator/vio_estimator.h>
 
 // TODO@mateosss: Reference in the readme that I'm compiling with
-// cmake .. -DCMAKE_INSTALL_PREFIX=$bsltinstall -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DEIGEN_ROOT=/usr/include/eigen3
-// cmake .. -DCMAKE_INSTALL_PREFIX=$bsltinstall -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DEIGEN_ROOT=/usr/include/eigen3
+// cmake . -B build -DCMAKE_INSTALL_PREFIX=$bsltinstall -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DEIGEN_ROOT=/usr/include/eigen3
 
 #define ASSERT(cond, ...)                                      \
   do {                                                         \
@@ -26,7 +26,32 @@
 namespace xrt::auxiliary::tracking::slam {
 
 using std::cout;
+using std::make_shared;
 using std::string;
+using std::to_string;
+using namespace basalt;
+
+string imu2str(const imu_sample &s) {
+  string str = "imu_sample ";
+  str += "t=" + to_string(s.timestamp) + " ";
+  str += "a=[" + to_string(s.ax) + ", " + to_string(s.ay) + ", " + to_string(s.az) + "] ";
+  str += "w=[" + to_string(s.wx) + ", " + to_string(s.wy) + ", " + to_string(s.wz) + "]";
+  return str;
+}
+
+string img2str(const img_sample &s) {
+  string str = "img_sample ";
+  str += s.is_left ? "left " : "right ";
+  str += "t=" + to_string(s.timestamp);
+  return str;
+}
+
+string pose2str(const pose &p) {
+  string str = "pose ";
+  str += "p=[" + to_string(p.px) + ", " + to_string(p.py) + ", " + to_string(p.pz) + "] ";
+  str += "r=[" + to_string(p.rx) + ", " + to_string(p.ry) + ", " + to_string(p.rz) + ", " + to_string(p.rw) + "]";
+  return str;
+}
 
 struct slam_tracker::implementation {
   // Options parsed from unified config file
@@ -44,11 +69,15 @@ struct slam_tracker::implementation {
   bool use_imu = true;
 
   // VIO members
-  basalt::Calibration<double> calib;
-  basalt::VioConfig vio_config;
-  basalt::OpticalFlowBase::Ptr opt_flow_ptr;
-  basalt::VioEstimatorBase::Ptr vio;
-  tbb::concurrent_bounded_queue<basalt::PoseVelBiasState<double>::Ptr> out_state_queue;
+  Calibration<double> calib;
+  VioConfig vio_config;
+  OpticalFlowBase::Ptr opt_flow_ptr;
+  VioEstimatorBase::Ptr vio;
+
+  // Queues
+  tbb::concurrent_bounded_queue<PoseVelBiasState<double>::Ptr> out_state_queue;
+  tbb::concurrent_bounded_queue<OpticalFlowInput::Ptr> *image_data_queue = nullptr;  // Invariant: not null after ctor
+  tbb::concurrent_bounded_queue<ImuData<double>::Ptr> *imu_data_queue = nullptr;     // Invariant: not null after ctor
 
   implementation(string unified_config) {
     // TODO@mateosss: [x] Argument parsing
@@ -80,16 +109,18 @@ struct slam_tracker::implementation {
     vio_config.load(config_path);
     load_calibration_data(cam_calib_path);
 
-    opt_flow_ptr = basalt::OpticalFlowFactory::getOpticalFlow(vio_config, calib);
-    vio = basalt::VioEstimatorFactory::getVioEstimator(vio_config, calib, basalt::constants::g, use_imu);
+    opt_flow_ptr = OpticalFlowFactory::getOpticalFlow(vio_config, calib);
+    vio = VioEstimatorFactory::getVioEstimator(vio_config, calib, constants::g, use_imu);
     vio->initialize(Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero());
 
+    imu_data_queue = &vio->imu_data_queue;
+    image_data_queue = &opt_flow_ptr->input_queue;
     opt_flow_ptr->output_queue = &vio->vision_data_queue;
     // if (show_gui) vio->out_vis_queue = &out_vis_queue; // TODO@mateosss: Use gui
     vio->out_state_queue = &out_state_queue;
 
-    printf(">>> all good\n");
-    exit(3);
+    ASSERT_(image_data_queue != nullptr);
+    ASSERT_(imu_data_queue != nullptr);
   }
 
   void load_unified_config(const string &unified_config) {
@@ -124,16 +155,9 @@ struct slam_tracker::implementation {
     cout << "Instantiating Basalt SLAM tracker\n";
     cout << "Using config file: " << app["--config"]->as<string>() << "\n";
     cout << app.config_to_str(true, true) << "\n";
-
-    // TODO@mateosss: remove these asserts related to config file parsing
-    ASSERT_(show_gui == true);
-    ASSERT_(dataset_path ==
-            "/home/mateo/Documents/apps/euroc/"
-            "2_mateosss_20210924_640x360_30fps_stereo_acc250_gyr200/");
-    ASSERT_(config_path == "/home/mateo/Documents/apps/bsltdeps/basalt/data/euroc_config.json");
   }
 
-  void load_calibration_data(const std::string &calib_path) {
+  void load_calibration_data(const string &calib_path) {
     std::ifstream os(calib_path, std::ios::binary);
     if (os.is_open()) {
       cereal::JSONInputArchive archive(os);
@@ -147,31 +171,88 @@ struct slam_tracker::implementation {
 
   void start() { printf(">>> implementation.%s\n", __func__); }
 
-  void stop() { printf(">>> implementation.%s\n", __func__); }
+  void stop() {
+    // Pushing nullptr signals end of stream
+    // TODO@mateosss: could it be a race condition here?
+    image_data_queue->push(nullptr);
+    imu_data_queue->push(nullptr);
+  }
 
   bool is_running() {
+    // TODO@mateosss: implement
     printf(">>> implementation.%s\n", __func__);
     return false;
   }
 
   void push_imu_sample(imu_sample s) {
-    (void)s;
-    // TODO@mateosss: use this queue for IMU samples
-    // t265_device->imu_data_queue = &vio->imu_data_queue;
-    printf(">>> implementation.%s\n", __func__);
+    // TODO@mateosss: remove print
+    // printf(">>> %s\n", imu2str(s).c_str());
+
+    // concurrent_bounded_queue expects Erasable and Allocator named
+    // requirements for the type, using a pointer because it already is. This is
+    // done in the others examples as well but it is far from optimal.
+    ImuData<double>::Ptr data;
+    data.reset(new ImuData<double>);
+    data->t_ns = s.timestamp;
+    data->accel = {s.ax, s.ay, s.az};
+    data->gyro = {s.wx, s.wy, s.wz};
+    imu_data_queue->push(data);
   }
 
+  OpticalFlowInput::Ptr partial_frame;
   void push_frame(img_sample s) {
-    (void)s;
-    // TODO@mateosss use this queue for pushing frames
-    // t265_device->image_data_queue = &opt_flow_ptr->input_queue;
-    printf(">>> implementation.%s\n", __func__);
+    // TODO@mateosss: remove print
+    // printf(">>> %s\n", img2str(s).c_str());
+
+    int i = -1;
+    if (s.is_left) {               // TODO@mateosss: Support mono?
+      constexpr int NUM_CAMS = 2;  // TODO@mateosss: add to config file?
+      partial_frame = make_shared<OpticalFlowInput>();
+      partial_frame->img_data.resize(NUM_CAMS);
+      partial_frame->t_ns = s.timestamp;
+      i = 0;
+    } else {
+      ASSERT(partial_frame->t_ns == s.timestamp, "Left and right frame timestamps differ: %ld != %ld",
+             partial_frame->t_ns, s.timestamp);
+      i = 1;
+    }
+
+    // TODO@mateosss: exposure?
+    // data->img_data[i].exposure = vf.get_frame_metadata(RS2_FRAME_METADATA_ACTUAL_EXPOSURE) * 1e-6;
+
+    int width = s.img.cols;
+    int height = s.img.rows;
+    // TODO@mateosss: why is it uint16_t instead of uint8_t?
+    auto &mimg = partial_frame->img_data[i].img;
+    mimg.reset(new ManagedImage<uint16_t>(width, height));
+
+    // TODO@mateosss: We should avoid this copy. See if ManagedImage can use a cv Mat somehow
+    size_t full_size = width * height;
+    for (size_t j = 0; j < full_size; j++) {
+      mimg->ptr[j] = s.img.data[j] << 8;
+    }
+
+    if (!s.is_left) {
+      image_data_queue->push(partial_frame);
+    }
   }
 
   bool try_dequeue_pose(pose &pose) {
-    (void)pose;
-    printf(">>> implementation.%s\n", __func__);
-    return false;
+    basalt::PoseVelBiasState<double>::Ptr data;
+    bool dequeued = out_state_queue.try_pop(data);
+    if (dequeued) {
+      Sophus::SE3d T_w_i = data->T_w_i;
+      pose.px = T_w_i.translation().x();
+      pose.py = T_w_i.translation().y();
+      pose.pz = T_w_i.translation().z();
+      pose.rx = T_w_i.unit_quaternion().x();
+      pose.ry = T_w_i.unit_quaternion().y();
+      pose.rz = T_w_i.unit_quaternion().z();
+      pose.rw = T_w_i.unit_quaternion().w();
+      // TODO@mateosss: remove print
+      printf(">>> try_dequeue_pose %s\n", pose2str(pose).c_str());
+    }
+    return dequeued;
   }
 };
 
