16467eebd3411db498eeb0f94fee2e0cca606d76
Mateo de Mayo <mateo.demayo@collabora.com>
2021-10-16 15:55:18 -0300

    Fix for tracked pose dequeue when tracker has not started

--- src/monado/slam_tracker.cpp
+++ src/monado/slam_tracker.cpp
@@ -10,6 +10,7 @@
 
 #include <cstdio>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <thread>
 #include "sophus/se3.hpp"
@@ -22,6 +23,8 @@
 // TODO@mateosss: Reference in the readme that I'm compiling with
 // cmake . -B build -DCMAKE_INSTALL_PREFIX=$bsltinstall -DCMAKE_BUILD_TYPE=RelWithDebInfo
 // -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DEIGEN_ROOT=/usr/include/eigen3 -DBUILD_TESTS=OFF
+// TODO@mateosss: Specify the need to add march=native when compiling monado with basalt
+// export CXXFLAGS=-march=native CFLAGS=-march=native
 
 namespace xrt::auxiliary::tracking::slam {
 
@@ -55,7 +58,6 @@ string pose2str(const pose &p) {
   str += "r=[" + to_string(p.rx) + ", " + to_string(p.ry) + ", " + to_string(p.rz) + ", " + to_string(p.rw) + "]";
   return str;
 }
-
 
 struct slam_tracker::implementation {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -278,9 +280,12 @@ struct slam_tracker::implementation {
 
   bool try_dequeue_pose(pose &pose) {
     if (latest_pose) {  // Return only the last produced pose
-      last_out_state_lock.lock();
+      std::unique_lock<std::mutex> lock{last_out_state_lock};
+
+      if (!last_out_state) return false;
       Sophus::SE3d T_w_i = last_out_state->T_w_i;
-      last_out_state_lock.unlock();
+      lock.unlock();
+
       pose.px = T_w_i.translation().x();
       pose.py = T_w_i.translation().y();
       pose.pz = T_w_i.translation().z();
