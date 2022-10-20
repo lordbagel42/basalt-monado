df81a8af3c05bf988e76782fbcdae8ca1accd871
Mateo de Mayo <mateo.demayo@collabora.com>
2022-10-20 11:58:09 -0300

    WIP: Adapt optflow strategies

--- include/basalt/optical_flow/multiscale_frame_to_frame_optical_flow.h
+++ include/basalt/optical_flow/multiscale_frame_to_frame_optical_flow.h
@@ -328,7 +328,7 @@ class MultiscaleFrameToFrameOpticalFlow : public OpticalFlowBase {
     return patch_valid;
   }
 
-  inline bool trackPointAtLevel(const Image<const uint16_t>& img_2,
+  inline bool trackPointAtLevel(const Image& img_2,
                                 const PatchT& dp,
                                 Eigen::AffineCompact2f& transform) const {
     bool patch_valid = true;
